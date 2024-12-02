2880a523ab90e8c8d9ec2e6edf9eb103285c4e1e
Anthony Abate <anthony.abate@gmail.com>
2024-12-02 15:23:47 -0500

    example unit test

---
+++ csharp/Savvy.ZooKeeper.Tests/Example.cs
@@ -0,0 +1,29 @@
+﻿namespace Savvy.ZooKeeper.Tests
+{
+    using Microsoft.EntityFrameworkCore;
+    using NSubstitute;
+    using Savvy.ZooKeeper.Controllers;
+    using Savvy.ZooKeeper.Models;
+    using Savvy.ZooKeeper.Services;
+
+    public class Example
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            // MOCK
+            var session = Substitute.For<IUserSession>();
+
+            // use in memory database
+            var options = new DbContextOptionsBuilder<ModelContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            var ac = new AnimalController(new ModelContext(options), session);
+
+            var rows = ac.Get(null, null);
+
+            Assert.That(rows, Is.Not.Null);
+        }
+    }
+}
