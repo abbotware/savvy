b32e931058398242b486831179793ba88c00e202
Anthony Abate <anthony.abate@gmail.com>
2024-12-01 20:58:58 -0500

    nav properties

--- csharp/ZooKeeper.Models/Security/PrincipalRole.cs
+++ csharp/ZooKeeper.Models/Security/PrincipalRole.cs
@@ -5,6 +5,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 [Table(nameof(PrincipalRole), Schema = Constants.SecuritySchema)]
 public class PrincipalRole : InsertableRecord
 {
+    [ForeignKey(nameof(Principal))]
+    public long PrincipalId { get; set; }
+
+    [ForeignKey(nameof(Role))]
+    public long RoleId { get; set; }
+
     public Principal Principal { get; set; } = null!;
 
     public Role Role { get; set; } = null!;
--- csharp/ZooKeeper.Models/Security/Role.cs
+++ csharp/ZooKeeper.Models/Security/Role.cs
@@ -1,8 +1,18 @@
 ﻿namespace Savvy.ZooKeeper.Models;
 
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 [Table(nameof(Role), Schema = Constants.SecuritySchema)]
 public class Role : NamedRecord
 {
+    [ForeignKey(nameof(PrincipalRoles))]
+    [Display(AutoGenerateField = false)]
+    [JsonIgnore]
+    public ICollection<Principal> Principals { get; } = [];
+
+    [Display(AutoGenerateField = false)]
+    [JsonIgnore]
+    public ICollection<PrincipalRole> PrincipalRoles { get; } = [];
 }
